3b1045120800f401ce6a773f80a8ba1673b62be9
OgembaJr <emmanuel.ogemba@student.moringaschool.com>
2023-02-12 10:26:23 +0300

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        array.filter { |letter| letter.split("").sort == word.split("").sort}
+    end
+end    
